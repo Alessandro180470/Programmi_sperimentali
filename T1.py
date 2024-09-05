a6f9e551d9eaf2a7362426971270eb6292f3bb48
Alessandro <126328633+Alessandro180470@users.noreply.github.com>
2024-09-06 00:50:27 +0200

    Programma aggiornato

--- T1.py
+++ T1.py
@@ -1,2 +1,14 @@
-from patologia_diabete1 import esegui_funzione
-esegui_funzione()#esegue la media della glicata e del ck
+from patologia_diabete1 import calcolo_valore, patologia_dt1
+
+analisi_ematiche = calcolo_valore()
+analisi1 = patologia_dt1(6.3,56,320,'2024/05/05')
+analisi_ematiche.append(analisi1)
+analisi_ematiche.append(patologia_dt1(6.1,51,540,'2024/05/21'))
+analisi_ematiche.append(patologia_dt1(6.8,49,320,'2024/05/23'))
+print(f'Il valore medio del ck è :{analisi_ematiche.media_ck()},mentre quello della glicata è :{analisi_ematiche.media_glicata()}')
+print(f'Il valore MAX e MIN della glicata sono: MAX {analisi_ematiche.val_max_glicata()} e MIN {analisi_ematiche.val_min_glicata()}')
+
+# analisi = patologia_dt1(float(input('Inserisci valore glicata: ')), float(input('Inserisci valore mmol: ')),
+#                float(input('Inserisci valore ck: ')), input('Inserisci la data delle analisi: '))
+# analisi1 =patologia_dt1(float(input('Inserisci valore glicata: ')),int(input('Inserisci valore mmol: ')),int(input('Inserisci valore ck: ')),input('Inserisci data del prelievo: '))
+
--- patologia_diabete1.py
+++ patologia_diabete1.py
@@ -16,9 +16,12 @@ class patologia_dt1:
             print(f'Attenzio:il valore del ck {self.ck} è alto ')
         if self.glicata == 0:
             raise ValueError(f'Attenzione:valore glicata {self.glicata} non valido')
+        if self.glicata > 6.7:
+            print(f'Attenzione hai un valore di glicata {self.glicata} inizio PREDIABETE')
 
     def __str__(self):
         return f'La glicata è: {self.glicata} con {self.mmol} mmol e un ck: {self.ck} analisi eseguita in data {self.data}'
+
 
 
 class calcolo_valore:
@@ -41,36 +44,26 @@ class calcolo_valore:
         risultato = [v.glicata for v in self.valore]
         return sum(risultato) / len(risultato)
 
+    def val_min_glicata(self):
+        risultato = [v.glicata for v in self.valore]
+        return min(risultato)
 
-# analisi = patologia_dt1(float(input('Inserisci valore glicata: ')), float(input('Inserisci valore mmol: ')),
-#                float(input('Inserisci valore ck: ')), input('Inserisci la data delle analisi: '))
-# analisi1 =patologia_dt1(float(input('Inserisci valore glicata: ')),int(input('Inserisci valore mmol: ')),int(input('Inserisci valore ck: ')),input('Inserisci data del prelievo: '))
-analisi = patologia_dt1(6.3, 51, 330, '2024/09/12')
-analisi1 = patologia_dt1(6.9, 55, 451, '2024/10/21')
-analisi2 = patologia_dt1(5.9, 51, 412, '2024/11/22')
-
-
-def calcola_valore_maggiore_minore_differenza():
-    val_max = max(analisi.ck, analisi1.ck, analisi2.ck)
-    val_min = min(analisi.ck, analisi1.ck, analisi2.ck)
-    diff = val_max - val_min
-    print(f'Il valore massimo riscontrato del CK rilevato nelle analisi è: {val_max}')
-    print(f'Mentre il valore minimo riscontrato è: {val_min}')
-    print(f'La differenza tra il valore max e il min è di {diff}')
-
-
-def esegui_funzione():
-    print(analisi)
-    print(analisi1)
-    miei_valori = calcolo_valore()
-    miei_valori.append(analisi)
-    miei_valori.append(analisi1)
-    print(f'Il valore medio del ck è: {miei_valori.media_ck()}')
-    print(f'Il valore medio della glicata è: {miei_valori.media_glicata()}')
-
-
+    def val_max_glicata(self):
+        risultato = [v.glicata for v in self.valore]
+        return max(risultato)
 
 if (__name__) == '__main__':
-    esegui_funzione()
-    calcola_valore_maggiore_minore_differenza()
-print('Il nome del modulo di importazione è :', __name__, )
+    print('Il nome del modulo di importazione è :', __name__, )
+else:
+    print('Il modulo importato è :',__name__)
+
+
+
+
+
+
+
+
+
+
+
